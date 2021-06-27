ec72f0b77c43b8506a8eccc3c5da3595e3d867b5
Tri Tran <tranminhtri9090@gmail.com>
2021-06-27 17:15:45 +1000

    (feat) Amplify Configuration

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "notes-app-upload-tritran",
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://krl9z0ig6c.execute-api.ap-southeast-2.amazonaws.com/prod/",
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_arTVuzWEl",
+      APP_CLIENT_ID: "4a4jf5nu8l0s3vve7b7v8ekl2s",
+      IDENTITY_POOL_ID: "ap-southeast-2:01bfa4dc-4f51-4ad4-beef-367fb11710d2",
+    },
+  };
+  
+  export default config;
