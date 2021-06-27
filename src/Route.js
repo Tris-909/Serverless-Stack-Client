18935ba2cb7a1acb4aef949a956de18c39cfdfc4
Tri Tran <tranminhtri9090@gmail.com>
2021-06-27 17:26:35 +1000

    (feat) Adding loginPage

--- src/Route.js
+++ src/Route.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
       <Route exact path="/">
         <Home />
       </Route>
