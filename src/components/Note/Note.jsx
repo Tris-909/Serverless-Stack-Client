5f793a897378d3560f7008f158fb93db71cde864
Tri Tran <tranminhtri9090@gmail.com>
2021-07-31 18:22:14 +1000

    (feat) Re-factor EditModal, moving all EditNote Logic outside of the note itself

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Image,
@@ -14,23 +14,11 @@ import EditNoteModal from "../NoteModal/EditNoteModal";
 import Draggable from "react-draggable";
 import { API } from "aws-amplify";
 import { CloseIcon, SettingsIcon, Search2Icon } from "@chakra-ui/icons";
-import { BsPencil } from "react-icons/bs";
-import config from "config";
-import { uploadToS3 } from "libs/awsLib";
-import { onError } from "libs/error-libs";
 import "./Note.scss";
 
 const Note = ({ note, deleteNote, fetchLists }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const initialRef = useRef();
-  const finalRef = useRef();
-
   const [position, setPosition] = useState({ x: note.x, y: note.y });
-  const [header, setHeader] = useState(note.header);
-  const [content, setContent] = useState(note.content);
-  const [previewImage, setPreviewImage] = useState(null);
-  const [deleteImage, setDeleteImage] = useState(false);
-  const file = useRef(null);
   const [onHide, setOnHide] = useState(true);
 
   const trackPosition = (data) => {
@@ -45,57 +33,6 @@ const Note = ({ note, deleteNote, fetchLists }) => {
         y: position.y,
       },
     });
-  };
-
-  const handleFileChange = (event) => {
-    file.current = event.target.files[0];
-    setPreviewImage(URL.createObjectURL(event.target.files[0]));
-    setDeleteImage(false);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let attachment;
-
-    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
-      alert(
-        `Please pick a file smaller than ${
-          config.MAX_ATTACHMENT_SIZE / 1000000
-        } MB.`
-      );
-      return;
-    }
-
-    try {
-      if (file.current.type.split("/")[0] === "image") {
-        attachment = await uploadToS3(file.current);
-      }
-
-      await API.put("notes", `/notes/${note.noteId}`, {
-        body: {
-          header,
-          content,
-          attachment: attachment || note.attachment,
-        },
-      });
-      onClose();
-      fetchLists();
-    } catch (e) {
-      onError(e);
-    }
-  };
-
-  const clearFileHandler = () => {
-    setPreviewImage(null);
-    file.current = {};
-    setDeleteImage(true);
-  };
-
-  const onEditHandler = () => {
-    setPreviewImage(null);
-    file.current = {};
-    setDeleteImage(false);
-    onOpen();
   };
 
   return (
@@ -124,12 +61,13 @@ const Note = ({ note, deleteNote, fetchLists }) => {
                 variant="none"
               />
               <MenuList>
-                <MenuItem
-                  icon={<BsPencil viewBox="0 0 15 15" />}
-                  onClick={() => onEditHandler()}
-                >
-                  Edit Note
-                </MenuItem>
+                <EditNoteModal
+                  note={note}
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  onOpen={onOpen}
+                  fetchLists={fetchLists}
+                />
                 <MenuItem
                   icon={<CloseIcon />}
                   onClick={() => deleteNote(note.noteId)}
@@ -195,23 +133,6 @@ const Note = ({ note, deleteNote, fetchLists }) => {
           </Box>
         </Box>
       </Draggable>
-      <EditNoteModal
-        header={header}
-        setHeader={setHeader}
-        initialRef={initialRef}
-        content={content}
-        setContent={setContent}
-        deleteImage={deleteImage}
-        previewImage={previewImage}
-        note={note}
-        clearFileHandler={clearFileHandler}
-        handleFileChange={handleFileChange}
-        handleSubmit={handleSubmit}
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      />
     </>
   );
 };
